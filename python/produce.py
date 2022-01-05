5dfad02e4850762a58b3b99e9997f366a7e5a28b
Bimal Poudel <anytizer@users.noreply.github.com>
2022-01-05 09:17:25 -0700

    Create produce.py

---
+++ python/produce.py
@@ -0,0 +1,9 @@
+@echo off
+
+python strokes2db.py
+python write_glyphs.py > _glyphs.py
+python write_strokes.py > _strokes.py
+python write_letters.py > _letters.py
+
+python compose.py
+python compose_merged.py > merged.birdfont
